1705c7dfe8569138e07ea3edef390c21178134ec
Ryan Kortvelesy <acciorocketships@gmail.com>
2016-09-17 08:00:17 -0400

    added wolfram and maps

--- speech.py
+++ speech.py
@@ -1,11 +1,34 @@
 import mechanize
 from wit import Wit
-from Tkinter import *
 import wolframalpha
 import speech_recognition as sr
-access_token = "Y2TXOOVIUF5444QL5U3OYV7MGRLRIJUW"
-client = Wit(access_token=access_token, actions={})
-wolframclient = wolframalpha.Client('AQV84A-YPJLV47P5P')
+import googlemaps
+import json
+import urllib2
+import gmplot
+from datetime import datetime
+witclient = Wit(access_token="Y2TXOOVIUF5444QL5U3OYV7MGRLRIJUW", actions={})
+wolframclient = wolframalpha.Client('KHE5G2-42E4UX5AL3')
+googleclient = googlemaps.Client(key='AIzaSyDpiP-1JYBIfuvfgmuDdxzhYX4FW-XmC0k')
+
+class Pod:
+    def __init__(self):
+        self.name = ""
+        self.text = ""
+        self.imgurl = ""
+
+class Witai:
+    def __init__(self):
+        self.query = ""
+        self.response = {}
+        self.intents = []
+        #self.arguments = []
+
+pods = []
+witai = Witai()
+
+
+# FUNCTIONS
 
 
 def text2speech(text, savefile):
@@ -40,6 +63,7 @@ def chatbot(text):
 	response = html[linkstart:linkend]
 	return response
 
+
 def speech2text():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
@@ -49,28 +73,51 @@ def speech2text():
 
 
 def getintents(query):
-    resp = client.message(query)
-    intents = []
-    if len(resp['entities']) != 0:
-        for intentnum in range(0,len(resp['entities']['intents'])):
-            intents.append(resp['entities']['intents'][intentnum]['value'])
-    return intents
+    witai.query = query
+    witai.response = witclient.message(query)
+    if 'intents' in witai.response['entities']:
+        for intentnum in range(0,len(witai.response['entities']['intents'])):
+            witai.intents.append(witai.response['entities']['intents'][intentnum]['value'])
 
-def runaction(intents,query):
-    for intent in intents:
-        actions[intent](query)    
+def runaction():
+    for intent in witai.intents:
+        actions[intent]()    
 
-def wolfram(query):
-    print(query)
+def wolfram():
+    response = wolframclient.query(witai.query)
+    for result in response.results:
+        pods.append(Pod())
+        pods[-1].name = result['@title']
+        pods[-1].text = result['subpod']['plaintext']
+        pods[-1].imgurl = result['subpod']['img']['@src']
 
 def directions():
-    print("Running directions action")
+    currentlocation = location_lookup()['loc']
+    lat = []
+    lng = []
+    instructions = []
+    lat.append(float(currentlocation[:currentlocation.find(",")-1]))
+    lng.append(float(currentlocation[currentlocation.find(",")+1:]))
+    directions = googleclient.directions(currentlocation,"Concord,MA",mode="driving")
+    gmap = gmplot.GoogleMapPlotter(lat[0],lng[0],16)
+    gmap.plot(lat,lng,'cornflowerblue',edge_width=10)
+    gmap.draw("mymap.html")
+
 
 def calendar():
     print("Running calendar action")
+
+
+# HELPER FUNCTIONS
 
 actions = {
     'Wolfram' : wolfram,
     'Directions' : directions,
     'Calendar' : calendar
 }
+
+def location_lookup():
+  try:
+    return json.load(urllib2.urlopen('http://ipinfo.io/json'))
+  except urllib2.HTTPError:
+    return False
--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 import speech
-import tungsten
 
-query = "What is the derivative of x squared"
-
+query = "Directions to 37th and market street"
 intents = speech.getintents(query)
-speech.runaction(intents,query)
-
-#speech.text2speech(speech.chatbot(query),True)
-#print(speech.speech2text())
-
+speech.runaction()
